913c8871991419a2a24c0b5589562aa5306bd79a
angusoehlers <37582379+angusoehlers@users.noreply.github.com>
2019-02-28 15:35:14 +0000

    register user DB

--- Dilemma/app/src/main/java/com/example/angus/dilemma/RegisterActivity.java
+++ Dilemma/app/src/main/java/com/example/angus/dilemma/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.angus.dilemma;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,9 +19,14 @@ public class RegisterActivity extends AppCompatActivity {
     EditText email, usern, pass1, pass2;
     TextView user_err, pass_err, email_err;
     Button register;
+    SQLiteDatabase db;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
+
+        db = new DBHandler(this).getWritableDatabase();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_r);
@@ -39,7 +46,15 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(checkFields()){
-                    registerUser();
+                    try {
+                        registerUser();
+                    }
+                    catch(NoSuchAlgorithmException e){
+                        e.printStackTrace();
+
+                    }catch(java.security.spec.InvalidKeySpecException i) {
+                        i.printStackTrace();
+                    }
 
                     Intent intent = new Intent();
                     intent.putExtra("user", usern.getText().toString());
@@ -109,13 +124,20 @@ public class RegisterActivity extends AppCompatActivity {
     private void registerUser() throws NoSuchAlgorithmException, InvalidKeySpecException {
         //get stuff into DB
         email.getText().toString();
-        usern.getText().toString();
+        String username =usern.getText().toString();
 
 
         Hashing hashed = new Hashing( pass1.getText().toString());
         String salt =hashed.getSalt().toString();
         String hashPass=hashed.getHash().toString();
 
+        ContentValues qValues = new ContentValues();
+        qValues.put("Username", username);
+        qValues.put("Password", hashPass);
+        qValues.put("Salt", salt);
+
+        // add ID, question, ans1, ans2, category (currently default)
+        long UserID = db.insert("User", null, qValues);
 
 
 
